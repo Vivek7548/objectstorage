be63a89377154edc5a7c30879cecbde92c85dab0
Vivek Ravindra Deshmukh <84085359+Vivek7548@users.noreply.github.com>
2023-10-13 23:36:52 +0530

    Create index.js
    adding this into objectstorage

---
+++ index.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Details</title>
+</head>
+
+<body>
+    <h1>User Details</h1>
+
+    <label for="username">Username:</label>
+    <input type="text" id="username">
+    <br><br>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email">
+    <br><br>
+
+    <button onclick="saveUserDetails()">Save Details</button>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
